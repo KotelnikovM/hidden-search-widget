560e682c0e19dff2bf4192fbc53ebce838011182
KotelnikovM <kotelnikovofficial@gmail.com>
2023-09-13 22:26:18 +0500

    add: add hidden-search-widget

---
+++ script.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('input');
+const search = document.querySelector('.search-image')
+search.addEventListener('click', () => {
+    input.classList.toggle('open')
+})
